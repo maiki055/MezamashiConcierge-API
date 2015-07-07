66567f72bf22e99fff9827e424f42f8d0935f222
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2015-07-08 01:24:03 +0900

    Create prefecture model

--- mezamashi-concierge/db/schema.rb
+++ mezamashi-concierge/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707081226) do
+ActiveRecord::Schema.define(version: 20150707162125) do
+
+  create_table "prefectures", force: :cascade do |t|
+    t.string   "name",       limit: 255, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "uuid",       limit: 255, null: false
