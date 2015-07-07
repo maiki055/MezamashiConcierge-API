f39199114c411d1acec60dfcf8fe9d26ef464150
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2015-07-07 13:17:18 +0900

    Enable to access v1 api

--- mezamashi-concierge/app/api/mezamashi_concierge/api.rb
+++ mezamashi-concierge/app/api/mezamashi_concierge/api.rb
@@ -1,4 +1,13 @@
 module MezamashiConcierge
   class API < Grape::API
+    version 'v1', using: :path
+    format :json
+    prefix :api
+
+    resource :alarms do
+      desc 'Return a my alarm.'
+      get :my_alarm do
+      end
+    end
   end
 end
